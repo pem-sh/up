3562bae57a93c9653b6389fe7ca0f2e0fb0f5999
Ethan Mick <ethan@ethanmick.com>
2025-02-26 21:45:47 -0500

    chore: add eslint to worker

---
+++ packages/worker/eslint.config.js
@@ -0,0 +1,20 @@
+import js from '@eslint/js'
+import globals from 'globals'
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      semi: ['error', 'never'],
+      quotes: ['error', 'single'],
+    },
+  },
+]
