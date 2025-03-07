56168e19ef5e4d519b33c10f7247e72272fe1eb1
Ethan Mick <ethan@ethanmick.com>
2025-03-07 15:20:26 -0500

    feat: protect routes with auth token (#8)

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,11 +1,30 @@
 import { withAuth } from 'next-auth/middleware'
+import { NextRequest, NextResponse } from 'next/server'
 
-export default withAuth({
-  pages: {
-    signIn: '/login',
-  },
-})
+const UP_ADMIN_TOKEN = process.env.UP_ADMIN_TOKEN
+
+export default async function middleware(request: NextRequest) {
+  if (request.nextUrl.pathname.startsWith('/v1')) {
+    const token = request.headers.get('X-UP-TOKEN')
+
+    if (token !== UP_ADMIN_TOKEN) {
+      return new NextResponse(null, { status: 401 })
+    }
+
+    return NextResponse.next()
+  }
+
+  // For all other routes, use the normal auth middleware
+  const auth = await withAuth({
+    pages: {
+      signIn: '/login',
+    },
+  })
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return (auth as any)(request)
+}
 
 export const config = {
-  matcher: ['/((?!api|_next|v1|register|login|$).*)'],
+  matcher: ['/((?!api|_next|register|login|$).*)'],
 }
